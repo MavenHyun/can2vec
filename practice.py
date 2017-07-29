c4d4c6924ac6ed86aea71df08c8fb53975133a6e
mavenhyun@gmail.com <smartboy7>
2017-07-29 22:01:47 +0900

    update

--- practice.py
+++ practice.py
@@ -157,19 +157,19 @@ class stacked_ae:
         input_mRNA = tf.placeholder("float", [self.X_mRNA.shape[0], self.X_mRNA.shape[1]])
         input_surviv = tf.placeholder("float", [self.Y.shape[0], self.Y.shape[1]])
         input_all = tf.concat([input_categ, input_mut, input_CNV, input_mRNA, input_surviv], 1)
-        answer = tf.placeholder("float", [self.X.shape[0], self.X.shape[1] + self.Y.shape[1]])
+        answer = tf.placeholder("float", [self.X.shape[0], self.Z.shape[1]])
 
         weights = {
             'W_enc1': tf.Variable(tf.random_normal([self.X.shape[1] + self.Y.shape[1], self.num_hnodes1])),
             'W_enc2': tf.Variable(tf.random_normal([self.num_hnodes1, self.num_hnodes2])),
             'W_dec2': tf.Variable(tf.random_normal([self.num_hnodes2, self.num_hnodes1])),
-            'W_dec1': tf.Variable(tf.random_normal([self.num_hnodes1, self.X.shape[1] + self.Y.shape[1]]))
+            'W_dec1': tf.Variable(tf.random_normal([self.num_hnodes1, self.Z.shape[1]]))
         }
         bias = {
             'B_enc1': tf.Variable(tf.random_normal([self.num_hnodes1])),
             'B_enc2': tf.Variable(tf.random_normal([self.num_hnodes2])),
             'B_dec2': tf.Variable(tf.random_normal([self.num_hnodes1])),
-            'B_dec1': tf.Variable(tf.random_normal([self.X.shape[1] + self.Y.shape[1]]))
+            'B_dec1': tf.Variable(tf.random_normal([self.Z.shape[1]]))
         }
 
         hidden1 = tf.nn.sigmoid(tf.add(tf.matmul(input_all, weights['W_enc1']), bias['B_enc1']))
@@ -202,18 +202,13 @@ class stacked_ae:
 
 
 
-class practice_autoencoder:
+class multi_ae:
 
-    def __init__(self, h1, h2, h3, h4, h5, h6, lr1, lr2, lr3, dn):
-        self.num_hnodes_mut1 = h1
-        self.num_hnodes_mut2 = h2
-        self.num_hnodes_CNV1 = h3
-        self.num_hnodes_CNV2 = h4
-        self.num_hnodes_mRNA1 = h5
-        self.num_hnodes_mRNA2 = h6
-        self.rate_learning1 = lr1
-        self.rate_learning2 = lr2
-        self.rate_learning3 = lr3
+    def __init__(self, h1, h2, h3, lr, dn):
+        self.num_hnodes_mut = h1
+        self.num_hnodes_CNV = h2
+        self.num_hnodes_mRNA = h3
+        self.rate_learning = lr
         self.denoise = dn
 
     def data_preprocess(self, file_name, file_name2):
@@ -241,15 +236,15 @@ class practice_autoencoder:
         for i in range(self.Y.shape[0]):
             self.Y[i, 0] = (self.Y[i, 0] - mean) / stdv
 
-        self.Z = np.concatenate((self.X_categ, self.X_mut, self.X_CNV, self.X_mRNA, self.Y), 1)
+        self.Z = self.Y
         if (self.denoise == True):
             self.X_categ = mask_noise(self.X_categ, 5)
             self.X_mut = mask_noise(self.X_mut, 50)
             self.X_CNV = mask_noise(self.X_CNV, 1000)
             self.X_mRNA = mask_noise(self.X_mRNA, 3000)
             self.Y = mask_noise(self.Y, 45)
-
-
+            self.num_hnodes = self.num_hnodes_mut + self.num_hnodes_CNV + self.num_hnodes_mRNA \
+                          + self.X_categ.shape[1] + self.Y.shape[1]
 
     def data_training(self, file_name, file_name2):
 
@@ -259,73 +254,65 @@ class practice_autoencoder:
         input_CNV = tf.placeholder("float", [self.X_CNV.shape[0], self.X_CNV.shape[1]])
         input_mRNA = tf.placeholder("float", [self.X_mRNA.shape[0], self.X_mRNA.shape[1]])
         input_surviv = tf.placeholder("float", [self.Y.shape[0], self.Y.shape[1]])
-        input_all = tf.concat([input_categ, input_mut, input_CNV, input_mRNA, input_surviv], 1)
-        answer = tf.placeholder("float", [self.X.shape[0], self.X.shape[1] + self.Y.shape[1]])
+        answer = tf.placeholder("float", [self.Y.shape[0], self.Y.shape[1]])
 
         weights = {
-            'W_mut_enc': tf.Variable(tf.random_normal([self.X_mut.shape[1], self.num_hnodes_mut1])),
-            'W_hidden1_mut_hidden2': tf.Variable(tf.random_normal([self.num_hnodes_mut1, self.num_hnodes_mut2])),
-            'W_hidden2_mut_output': tf.Variable(tf.random_normal([self.num_hnodes_mut2, self.X_mut.shape[1]])),
-            'W_input_CNV_hidden1': tf.Variable(tf.random_normal([self.X_CNV.shape[1], self.num_hnodes_CNV1])),
-            'W_hidden1_CNV_hidden2': tf.Variable(tf.random_normal([self.num_hnodes_CNV1, self.num_hnodes_CNV2])),
-            'W_hidden2_CNV_output': tf.Variable(tf.random_normal([self.num_hnodes_CNV2, self.X_CNV.shape[1]])),
-            'W_input_mRNA_hidden1': tf.Variable(tf.random_normal([self.X_mRNA.shape[1], self.num_hnodes_mRNA1])),
-            'W_hidden1_mRNA_hidden2': tf.Variable(tf.random_normal([self.num_hnodes_mRNA1, self.num_hnodes_mRNA2])),
-            'W_hidden2_mRNA_output': tf.Variable(tf.random_normal([self.num_hnodes_mRNA2, self.X_mRNA.shape[1]]))
+            'W_mut_enc': tf.Variable(tf.random_normal([self.X_mut.shape[1], self.num_hnodes_mut])),
+            'W_CNV_enc': tf.Variable(tf.random_normal([self.X_CNV.shape[1], self.num_hnodes_CNV])),
+            'W_mRNA_enc': tf.Variable(tf.random_normal([self.X_mRNA.shape[1], self.num_hnodes_mRNA])),
+            'W_mut_dec': tf.Variable(tf.random_normal([self.num_hnodes_mut, self.X_mut.shape[1]])),
+            'W_CNV_dec': tf.Variable(tf.random_normal([self.num_hnodes_CNV, self.X_CNV.shape[1]])),
+            'W_mRNA_dec': tf.Variable(tf.random_normal([self.num_hnodes_mRNA, self.X_mRNA.shape[1]])),
+            'W_dec': tf.Variable(tf.random_normal([self.num_hnodes, self.Z.shape[1]]))
         }
 
         bias = {
-            'B_input_mut_hidden1' : tf.Variable(tf.random_normal([self.num_hnodes_mut1])),
-            'B_hidden1_mut_hidden2' : tf.Variable(tf.random_normal([self.num_hnodes_mut2])),
-            'B_hidden2_mut_output' : tf.Variable(tf.random_normal([self.X_mut.shape[1]])),
-            'B_input_CNV_hidden1': tf.Variable(tf.random_normal([self.num_hnodes_CNV1])),
-            'B_hidden1_CNV_hidden2': tf.Variable(tf.random_normal([self.num_hnodes_CNV2])),
-            'B_hidden2_CNV_output': tf.Variable(tf.random_normal([self.X_CNV.shape[1]])),
-            'B_input_mRNA_hidden1': tf.Variable(tf.random_normal([self.num_hnodes_mRNA1])),
-            'B_hidden1_mRNA_hidden2': tf.Variable(tf.random_normal([self.num_hnodes_mRNA2])),
-            'B_hidden2_mRNA_output': tf.Variable(tf.random_normal([self.X_mRNA.shape[1]]))
+            'B_mut_enc': tf.Variable(tf.random_normal([self.num_hnodes_mut])),
+            'B_CNV_enc': tf.Variable(tf.random_normal([self.num_hnodes_CNV])),
+            'B_mRNA_enc': tf.Variable(tf.random_normal([self.num_hnodes_mRNA])),
+            'B_mut_dec': tf.Variable(tf.random_normal([self.X_mut.shape[1]])),
+            'B_CNV_dec': tf.Variable(tf.random_normal([self.X_CNV.shape[1]])),
+            'B_mRNA_dec': tf.Variable(tf.random_normal([self.X_mRNA.shape[1]])),
+            'B_dec': tf.Variable(tf.random_normal([self.Z.shape[1]]))
         }
 
-        hidden1_mut = tf.nn.sigmoid(tf.add(tf.matmul(input_mut, weights['W_input_mut_hidden1']), bias['B_input_mut_hidden1']))
-        hidden2_mut = tf.nn.sigmoid(tf.add(tf.matmul(hidden1_mut, weights['W_hidden1_mut_hidden2']), bias['B_hidden1_mut_hidden2']))
-        output_mut = tf.nn.sigmoid(tf.add(tf.matmul(hidden2_mut, weights['W_hidden2_mut_output']), bias['B_hidden2_mut_output']))
-
-        hidden1_CNV = tf.nn.sigmoid(tf.add(tf.matmul(input_CNV, weights['W_input_CNV_hidden1']), bias['B_input_CNV_hidden1']))
-        hidden2_CNV = tf.nn.sigmoid(tf.add(tf.matmul(hidden1_CNV, weights['W_hidden1_CNV_hidden2']), bias['B_hidden1_CNV_hidden2']))
-        output_CNV = tf.nn.sigmoid(tf.add(tf.matmul(hidden2_CNV, weights['W_hidden2_CNV_output']), bias['B_hidden2_CNV_output']))
-        
-        hidden1_mRNA = tf.nn.sigmoid(tf.add(tf.matmul(input_mRNA, weights['W_input_mRNA_hidden1']), bias['B_input_mRNA_hidden1']))
-        hidden2_mRNA = tf.nn.sigmoid(tf.add(tf.matmul(hidden1_mRNA, weights['W_hidden1_mRNA_hidden2']), bias['B_hidden1_mRNA_hidden2']))
-        output_mRNA = tf.nn.sigmoid(tf.add(tf.matmul(hidden2_mRNA, weights['W_hidden2_mRNA_output']), bias['B_hidden2_mRNA_output']))
-
+        hidden_mut = tf.nn.sigmoid(tf.add(tf.matmul(input_mut, weights['W_mut_enc']), bias['B_mut_enc']))
+        output_mut = tf.nn.sigmoid(tf.add(tf.matmul(hidden_mut, weights['W_mut_dec']), bias['B_mut_dec']))
+        hidden_CNV = tf.nn.sigmoid(tf.add(tf.matmul(input_CNV, weights['W_CNV_enc']), bias['B_CNV_enc']))
+        output_CNV = tf.nn.sigmoid(tf.add(tf.matmul(hidden_CNV, weights['W_CNV_dec']), bias['B_CNV_dec']))
+        hidden_mRNA = tf.nn.sigmoid(tf.add(tf.matmul(input_mRNA, weights['W_mRNA_enc']), bias['B_mRNA_enc']))
+        output_mRNA = tf.nn.sigmoid(tf.add(tf.matmul(hidden_mRNA, weights['W_mRNA_dec']), bias['B_mRNA_dec']))
+        feature = tf.concat([input_categ, hidden_mut, hidden_CNV, hidden_mRNA, input_surviv], 1)
+        hidden = tf.nn.sigmoid(tf.add(tf.matmul(feature, weights['W_dec']), bias['B_dec']))
+        hidden0, output = tf.split(hidden, [self.Z.shape[1] - 1, 1], 1)
+        '''
         cost_mut = tf.reduce_mean(tf.pow(input_mut - output_mut, 2))
-        optimizer_mut = tf.train.GradientDescentOptimizer(self.rate_learning1).minimize(cost_mut)
+        optimizer_mut = tf.train.RMSPropOptimizer(self.rate_learning).minimize(cost_mut)
         cost_CNV = tf.reduce_mean(tf.pow(input_CNV - output_CNV, 2))
-        optimizer_CNV = tf.train.GradientDescentOptimizer(self.rate_learning1).minimize(cost_CNV)
+        optimizer_CNV = tf.train.RMSPropOptimizer(self.rate_learning).minimize(cost_CNV)
         cost_mRNA = tf.reduce_mean(tf.pow(input_mRNA - output_mRNA, 2))
-        optimizer_mRNA = tf.train.GradientDescentOptimizer(self.rate_learning1).minimize(cost_mRNA)
+        optimizer_mRNA = tf.train.RMSPropOptimizer(self.rate_learning).minimize(cost_mRNA)
+        '''
+
+
+        cost_surviv = tf.reduce_mean(tf.pow(answer - output, 2))
+        optimizer_surviv = tf.train.RMSPropOptimizer(self.rate_learning).minimize(cost_surviv)
 
         init = tf.global_variables_initializer()
         sess = tf.Session()
         sess.run(init)
 
-        """
-        for epoch in range(400):
-            for i in range(256):
-                batch_xs, batch_ys = mnist.train.next_batch(256)
-                _, cost_value = sess.run([optimizer, cost], feed_dict={input: batch_xs})
-            if epoch % 1 == 0:
-                print("Epoch:", '%04d' % (epoch + 1), "cost=", "{:.9f}".format(cost_value))
-        print("Stacked Autoencoder pre-training Optimization Finished!")
-        """
-
-        for iter in range(10000):
-           cost1, cost2, cost3, _, _, _ = sess.run([cost_mut, cost_CNV, cost_mRNA, optimizer_mut, optimizer_CNV, optimizer_mRNA],
-                                    feed_dict={input_mut: self.X_mut, input_CNV: self.X_CNV, input_mRNA: self.X_mRNA})
+        for iter in range(5000):
+           #cost1, cost2, cost3, cost4, _, _, _, _ = sess.run([cost_mut, cost_CNV, cost_mRNA, cost_surviv, optimizer_mut, optimizer_CNV, optimizer_mRNA,  optimizer_surviv],
+           cost_value, _ = sess.run([cost_surviv, optimizer_surviv],
+                                    feed_dict={input_categ: self.X_categ, input_mut: self.X_mut,
+                                               input_CNV: self.X_CNV, input_mRNA: self.X_mRNA,
+                                               input_surviv: self.Y, answer: self.Z})
            if (iter % 100 == 0):
-               print(iter,  "\tMutation Data\t", cost1, "\tCNV Data\t", cost2, "\tmRNA Data\t", cost3)
+               #print(iter,  "\tMutation Data\t", cost1, "\tCNV Data\t", cost2, "\tmRNA Data\t", cost3, "\tSurvivability\t", cost4)
+               print(iter, "cost is ", cost_value)
 
-test = stacked_ae(300, 100, 0.8, True)
+test = multi_ae(20, 100, 500, 0.99, True)
 test.data_training('ACC_features.tsv', 'ACC_survival.tsv')
 
 
