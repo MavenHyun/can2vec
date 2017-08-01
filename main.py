ec4856bd988f556b586a6e728cd0732b0629f456
mavenhyun@gmail.com <smartboy7>
2017-08-01 12:48:42 +0900

    modified multi_ae 2

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ import multi_ae as ma
 
 tr = ma.train_set('ACC_features.tsv', 'ACC_survival.tsv')
 tr.data_preprocess()
-maven = ma.multi_ae(tr, 10, 20, 100, 500, 0.80)
-maven.tf_construct(tr)
-maven.pre_train()
-maven.data_train()
+maven = ma.multi_ae(tr, 10, 20, 100, 800, 0.001)
+maven.execute(tr)
 
 
--- multi_ae.py
+++ multi_ae.py
@@ -4,10 +4,11 @@ import numpy as np
 import pandas as pd
 import statistics as st
 
+
 class train_set:
     def __init__(self, fn, fn2):
         self.file_name, self.file_name2 = fn, fn2
-        
+
     def data_preprocess(self):
         df = pd.read_csv(self.file_name, '\t')
         df.fillna(0, inplace=True)
@@ -34,6 +35,7 @@ class train_set:
             self.Y[i, 0] = (self.Y[i, 0] - mean) / stdv
 
         self.Z = np.concatenate((self.X, self.Y), 1)
+
 
 class multi_ae:
     def __init__(self, set, h1, h2, h3, h4, lr):
@@ -74,16 +76,24 @@ class multi_ae:
             'B_dec': tf.Variable(tf.random_normal([self.Z.shape[1]]))
         }
         self.hiddens = {
-            'H_categ': tf.nn.sigmoid(tf.add(tf.matmul(self.input_categ, self.weights['W_categ_enc']), self.bias['B_categ_enc'])),
-            'H_mut': tf.nn.sigmoid(tf.add(tf.matmul(self.input_mut, self.weights['W_mut_enc']), self.bias['B_mut_enc'])),
-            'H_CNV': tf.nn.sigmoid(tf.add(tf.matmul(self.input_CNV, self.weights['W_CNV_enc']), self.bias['B_CNV_enc'])),
-            'H_mRNA': tf.nn.sigmoid(tf.add(tf.matmul(self.input_mRNA, self.weights['W_mRNA_enc']), self.bias['B_mRNA_enc']))
+            'H_categ': tf.nn.sigmoid(
+                tf.add(tf.matmul(self.input_categ, self.weights['W_categ_enc']), self.bias['B_categ_enc'])),
+            'H_mut': tf.nn.sigmoid(
+                tf.add(tf.matmul(self.input_mut, self.weights['W_mut_enc']), self.bias['B_mut_enc'])),
+            'H_CNV': tf.nn.sigmoid(
+                tf.add(tf.matmul(self.input_CNV, self.weights['W_CNV_enc']), self.bias['B_CNV_enc'])),
+            'H_mRNA': tf.nn.sigmoid(
+                tf.add(tf.matmul(self.input_mRNA, self.weights['W_mRNA_enc']), self.bias['B_mRNA_enc']))
         }
         self.outputs = {
-            'output_categ': tf.nn.sigmoid(tf.add(tf.matmul(self.hiddens['H_categ'], self.weights['W_categ_dec']), self.bias['B_categ_dec'])),
-            'output_mut': tf.nn.sigmoid(tf.add(tf.matmul(self.hiddens['H_mut'], self.weights['W_mut_dec']), self.bias['B_mut_dec'])),
-            'output_CNV': tf.nn.sigmoid(tf.add(tf.matmul(self.hiddens['H_CNV'], self.weights['W_CNV_dec']), self.bias['B_CNV_dec'])),
-            'output_mRNA': tf.nn.sigmoid(tf.add(tf.matmul(self.hiddens['H_mRNA'], self.weights['W_mRNA_dec']), self.bias['B_mRNA_dec']))
+            'output_categ': tf.nn.sigmoid(
+                tf.add(tf.matmul(self.hiddens['H_categ'], self.weights['W_categ_dec']), self.bias['B_categ_dec'])),
+            'output_mut': tf.nn.sigmoid(
+                tf.add(tf.matmul(self.hiddens['H_mut'], self.weights['W_mut_dec']), self.bias['B_mut_dec'])),
+            'output_CNV': tf.nn.sigmoid(
+                tf.add(tf.matmul(self.hiddens['H_CNV'], self.weights['W_CNV_dec']), self.bias['B_CNV_dec'])),
+            'output_mRNA': tf.nn.sigmoid(
+                tf.add(tf.matmul(self.hiddens['H_mRNA'], self.weights['W_mRNA_dec']), self.bias['B_mRNA_dec']))
         }
         self.costs = {
             'cost_categ': tf.reduce_mean(tf.pow(self.input_categ - self.outputs['output_categ'], 2)),
@@ -92,13 +102,14 @@ class multi_ae:
             'cost_mRNA': tf.reduce_mean(tf.pow(self.input_mRNA - self.outputs['output_mRNA'], 2))
         }
         self.optis = {
-            'opti_categ':tf.train.RMSPropOptimizer(self.rate_learning).minimize(self.costs['cost_categ']),
+            'opti_categ': tf.train.RMSPropOptimizer(self.rate_learning).minimize(self.costs['cost_categ']),
             'opti_mut': tf.train.RMSPropOptimizer(self.rate_learning).minimize(self.costs['cost_mut']),
             'opti_CNV': tf.train.RMSPropOptimizer(self.rate_learning).minimize(self.costs['cost_CNV']),
             'opti_mRNA': tf.train.RMSPropOptimizer(self.rate_learning).minimize(self.costs['cost_mRNA'])
         }
 
-        feature = tf.concat([self.hiddens['H_categ'], self.hiddens['H_mut'], self.hiddens['H_CNV'], self.hiddens['H_mRNA']], 1)
+        feature = tf.concat(
+            [self.hiddens['H_categ'], self.hiddens['H_mut'], self.hiddens['H_CNV'], self.hiddens['H_mRNA']], 1)
         output = tf.nn.sigmoid(tf.add(tf.matmul(feature, self.weights['W_dec']), self.bias['B_dec']))
         output0, output1 = tf.split(output, [self.Z.shape[1] - 1, 1], 1)
         self.cost = tf.reduce_mean(tf.pow(self.input_surviv - output1, 2))
@@ -110,12 +121,13 @@ class multi_ae:
         sess.run(init)
         for iter in range(2):
             _, _, _, _, _, _, _, _ = sess.run([self.costs['cost_categ'], self.optis['opti_categ'],
-                             self.costs['cost_mut'], self.optis['opti_mut'],
-                             self.costs['cost_CNV'], self.optis['opti_CNV'],
-                             self.costs['cost_mRNA'], self.optis['opti_mRNA']], feed_dict={self.input_categ: self.X_categ,
-                                                                                           self.input_mut: self.X_mut,
-                                                                                           self.input_CNV: self.X_CNV,
-                                                                                           self.input_mRNA: self.X_mRNA})
+                                               self.costs['cost_mut'], self.optis['opti_mut'],
+                                               self.costs['cost_CNV'], self.optis['opti_CNV'],
+                                               self.costs['cost_mRNA'], self.optis['opti_mRNA']],
+                                              feed_dict={self.input_categ: self.X_categ,
+                                                         self.input_mut: self.X_mut,
+                                                         self.input_CNV: self.X_CNV,
+                                                         self.input_mRNA: self.X_mRNA})
         print("Optimization done!")
 
     def data_train(self):
@@ -125,10 +137,10 @@ class multi_ae:
 
         for iter in range(5001):
             c, _ = sess.run([self.cost, self.opt], feed_dict={self.input_categ: self.X_categ,
-                                                            self.input_mut: self.X_mut,
-                                                            self.input_CNV: self.X_CNV,
-                                                            self.input_mRNA: self.X_mRNA,
-                                                            self.input_surviv: self.Y})
+                                                              self.input_mut: self.X_mut,
+                                                              self.input_CNV: self.X_CNV,
+                                                              self.input_mRNA: self.X_mRNA,
+                                                              self.input_surviv: self.Y})
             if iter % 100 == 0:
                 print(iter, "Cost is ", c)
 
