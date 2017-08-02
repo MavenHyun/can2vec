e01d09f82533bf3cbe75630057ebd0a1ca952abe
mavenhyun@gmail.com <smartboy7>
2017-08-02 16:29:41 +0900

    modified split_ae

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ tr.data_split()
 sa1 = sa.split_ae(tr.X_cli, tr.X_cli_eval, tr.X_cli_test, 10, 0.01)
 sa1.initiate()
 
-sa2 = sa.split_ae(tr.X_mut, tr.X_mut_eval, tr.X_mut_test, 20, 0.01)
+sa2 = sa.split_ae(tr.X_mut, tr.X_mut_eval, tr.X_mut_test, 20, 0.007)
 sa2.initiate()
 
-sa3 = sa.split_ae(tr.X_CNV, tr.X_CNV_eval, tr.X_CNV_test, 300, 0.01)
+sa3 = sa.split_ae(tr.X_CNV, tr.X_CNV_eval, tr.X_CNV_test, 300, 0.005)
 sa3.initiate()
 
-sa4 = sa.split_ae(tr.X_mRNA, tr.X_mRNA_eval, tr.X_mRNA_test, 800, 0.01)
+sa4 = sa.split_ae(tr.X_mRNA, tr.X_mRNA_eval, tr.X_mRNA_test, 800, 0.005)
 sa4.initiate()
 
 sa1.printout()
