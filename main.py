67d3ff1cd389b7409620113c56232ba45ce3bf9f
mavenhyun@gmail.com <smartboy7>
2017-08-03 16:45:26 +0900

    gogogo

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ for iter in range(10):
     sa1.initiate()
     sa2 = sa.split_ae(tr.x_mut, tr.X_mut_eval, tr.X_mut_test, 20, 0.1)
     sa2.initiate()
-    sa3 = sa.split_ae(tr.x_CNV, tr.X_CNV_eval, tr.X_CNV_test, 500, 0.3)
+    sa3 = sa.split_ae(tr.x_CNV, tr.X_CNV_eval, tr.X_CNV_test, 600, 0.4)
     sa3.initiate()
-    #sa4 = sa.split_ae(tr.x_mRNA, tr.X_mRNA_eval, tr.X_mRNA_test, 1000, 0.1)
-    #sa4.initiate()
+    sa4 = sa.split_ae(tr.x_mRNA, tr.X_mRNA_eval, tr.X_mRNA_test, 1000, 0.3)
+    sa4.initiate()
     sa1.printout()
     sa2.printout()
     sa3.printout()
-    #sa4.printout()
+    sa4.printout()
 
 '''
 maven = ma.multi_ae(tr, sa1, sa2, sa3, sa4, 0.001)
