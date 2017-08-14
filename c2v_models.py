d1146d6b66a0ad89eda767893938b898188a078a
mavenhyun@gmail.com <smartboy7>
2017-08-14 10:22:07 +0900

    revamp

--- c2v_models.py
+++ c2v_models.py
@@ -187,9 +187,8 @@ class FarSeer:
                 for iter in range(epochs):
                     train_cost, _ = sess.run([cost, opti], feed_dict=self.train_dict)
                     vali_cost = sess.run(cost, feed_dict=self.vali_dict)
-                    if iter % (epochs / 100) == 0:
-                        learn = grey_magic(learn, train_cost, old_train)
-                        print("Feature: ", fea, iter, "Training Cost: ", train_cost, "Evaluation Cost: ", vali_cost)
+                    learn = grey_magic(learn, train_cost, old_train)
+                    print("Feature: ", fea, iter, "Training Cost: ", train_cost, "Evaluation Cost: ", vali_cost)
                     if red_magic(learn, old_train, train_cost, old_vali, vali_cost, iter) is True:
                         break
                     old_train = train_cost
