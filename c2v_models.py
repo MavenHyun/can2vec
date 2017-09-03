46957eda629ded8a515d678d614ee22e53e6493d
Maven Hyun <mavenhyun@gmail.com>
2017-09-03 11:36:41 +0900

    added cox

--- c2v_models.py
+++ c2v_models.py
@@ -311,7 +311,7 @@ class FarSeer:
                 for iter in range(epochs):
                     surv_time = sess.run([self.P['sur']], feed_dict=self.train_dict)
                     partial_sum = self.cox_cummulative(result, surv_time)
-                    cost = -tf.reduce_sum(tf.subtract(result, partial_sum) * self.C['train'])
+                    cost = -(tf.reduce_sum(tf.subtract(result, partial_sum) * self.C['train']))
                     opti = white_magic(meth, learn, cost)
                     c, _, summ, surv_pred, surv_real = sess.run([cost, opti, merged, result, self.P['sur']],
                                                                 feed_dict=self.train_dict)
