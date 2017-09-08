7d820417febea0094cc4e4748ef37ee7fe812cb5
mavenhyun@gmail.com <smartboy7>
2017-09-08 18:22:06 +0900

    aaa

--- model_builder.py
+++ model_builder.py
@@ -46,7 +46,7 @@ def create_model(pretrain):
     with tf.name_scope("Survival_Prediction"):
         pro = maven.data_projector(vector, 619, 619, 'relu')
         pre = maven.surv_predictor(pro, 619, 'relu')
-        maven.optimize_CPredictor(pre, 10001, 1e-15)
+        maven.optimize_CPredictor(pre, 10001, 1e-6)
         maven.optimize_SPredictor(pre, 'adam', 10001, 1e-6)
 
 
