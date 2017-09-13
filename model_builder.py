714101d90301ce176aca02da1d4fa556e3cf7ca3
mavenhyun@gmail.com <smartboy7>
2017-09-13 13:26:52 +0900

    aaa

--- model_builder.py
+++ model_builder.py
@@ -43,8 +43,8 @@ def create_model(pretrain):
     with tf.name_scope("Survival_Prediction"):
         vector = tf.concat([mRNA, CNV, mut, cli], 0)
         pro = maven.data_projector(vector, 619, 1000, 'relu')
-        #pre = maven.surv_predictor(pro, 'relu')
-        #maven.optimize_SPredictor(pre, 'adag', 5001, 1e-3)
-        rec = maven.re_constructor(pro, 'raw')
-        maven.optimize_RConstructor(rec, 'adam', 5001, 1e-3)
+        pre = maven.surv_predictor(pro, 'relu')
+        maven.optimize_SPredictor(pre, 'adag', 5001, 1e-3)
+        #rec = maven.re_constructor(pro, 'relu')
+        #maven.optimize_RConstructor(rec, 'adam', 5001, 1e-3)
 
